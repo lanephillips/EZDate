08747f159bf5b2d9a25d4b2a925f165ce792750f
Lane Phillips <lane@milkllc.com>
2013-06-29 22:12:57 -0500

    example project

--- EZDateExamples/EZDateExamples/LPExamples1ViewController.m
+++ EZDateExamples/EZDateExamples/LPExamples1ViewController.m
@@ -23,10 +23,70 @@
     [super viewDidLoad];
 
     _codes = @[
-                   @"[EZDate date]"
+                 @"[EZDate date]",
+                 @"[EZDate dateWithTimeIntervalSinceNow:12345678]",
+                 @"[EZDate dateWithTimeIntervalSinceReferenceDate:12345678]",
+                 @"[EZDate dateWithTimeIntervalSince1970:12345678]",
+                 @"[EZDate dateWithTimeInterval:12345678 sinceDate:[NSDate date]]",
+                 @"[EZDate distantFuture]",
+                 @"[EZDate distantPast]",
+                 @"[[EZDate alloc] init]",
+                 @"[[EZDate alloc] initWithTimeIntervalSinceNow:12345678]",
+                 @"[[EZDate alloc] initWithTimeIntervalSinceReferenceDate:12345678]",
+                 @"[[EZDate alloc] initWithTimeIntervalSince1970:12345678]",
+                 @"[[EZDate alloc] initWithTimeInterval:12345678 sinceDate:[NSDate date]]",
+                 @"[EZDate dateWithNSDate:[NSDate date]]",
+                 @"[[EZDate alloc] initWithNSDate:[NSDate date]]",
+                 @"[EZDate dateWithNSDate:[NSDate date] calendarIdentifier:NSGregorianCalendar timeZone:[NSTimeZone defaultTimeZone]]",
+                 @"[[EZDate alloc] initWithNSDate:[NSDate date] calendarIdentifier:NSGregorianCalendar timeZone:[NSTimeZone defaultTimeZone]]",
+                 @"[EZDate dateWithYear:2013 month:7 day:4]",
+                 @"[EZDate dateWithYear:2013 month:7 day:4 hour:22 minute:5 second:7]",
+                 @"[EZDate dateWithHour:8 minute:33 second:47]",
+                 @"[EZDate dateWithYear:2012 month:11 ordinal:1 weekday:2]",
+                 @"[[EZDate date] dateByAddingYears:1 months:1 days:-1]",
+                 @"[[EZDate date] dateByAddingHours:-1 minutes:1 seconds:20]",
+                 @"[[EZDate date] dateByAddingTimeInterval:12345678]",
+                 @"[[EZDate date] description]",
+                 @"[[EZDate date] dateAtNextOccurrenceOfWeekday:6]",
+                 @"[[EZDate date] dateAtNextOccurrenceOfMonth:7 day:4]",
+                 @"[[EZDate date] dateAtNextOccurrenceOfHour:6 minute:45]",
+                 @"[EZDate date][@\"EEEE, MMMM d, y\"]",
+                 @"[[EZDate date] stringWithDateFormat:@\"EEEE, MMMM d, y\"]",
+                 @"[[EZDate date] prettyString]",
+                 @"[[EZDate dateWithYear:2013 month:7 day:4] prettyElapsedTimeString]"
                    ];
     _results = @[
-                     [EZDate date]
+                 [EZDate date],
+                 [EZDate dateWithTimeIntervalSinceNow:12345678],
+                 [EZDate dateWithTimeIntervalSinceReferenceDate:12345678],
+                 [EZDate dateWithTimeIntervalSince1970:12345678],
+                 [EZDate dateWithTimeInterval:12345678 sinceDate:[NSDate date]],
+                 [EZDate distantFuture],
+                 [EZDate distantPast],
+                 [[EZDate alloc] init],
+                 [[EZDate alloc] initWithTimeIntervalSinceNow:12345678],
+                 [[EZDate alloc] initWithTimeIntervalSinceReferenceDate:12345678],
+                 [[EZDate alloc] initWithTimeIntervalSince1970:12345678],
+                 [[EZDate alloc] initWithTimeInterval:12345678 sinceDate:[NSDate date]],
+                 [EZDate dateWithNSDate:[NSDate date]],
+                 [[EZDate alloc] initWithNSDate:[NSDate date]],
+                 [EZDate dateWithNSDate:[NSDate date] calendarIdentifier:NSGregorianCalendar timeZone:[NSTimeZone defaultTimeZone]],
+                 [[EZDate alloc] initWithNSDate:[NSDate date] calendarIdentifier:NSGregorianCalendar timeZone:[NSTimeZone defaultTimeZone]],
+                 [EZDate dateWithYear:2013 month:7 day:4],
+                 [EZDate dateWithYear:2013 month:7 day:4 hour:22 minute:5 second:7],
+                 [EZDate dateWithHour:8 minute:33 second:47],
+                 [EZDate dateWithYear:2012 month:11 ordinal:1 weekday:2],
+                 [[EZDate date] dateByAddingYears:1 months:1 days:-1],
+                 [[EZDate date] dateByAddingHours:-1 minutes:1 seconds:20],
+                 [[EZDate date] dateByAddingTimeInterval:12345678],
+                 [[EZDate date] description],
+                 [[EZDate date] dateAtNextOccurrenceOfWeekday:6],
+                 [[EZDate date] dateAtNextOccurrenceOfMonth:7 day:4],
+                 [[EZDate date] dateAtNextOccurrenceOfHour:6 minute:45],
+                 [EZDate date][@"EEEE, MMMM d, y"],
+                 [[EZDate date] stringWithDateFormat:@"EEEE, MMMM d, y"],
+                 [[EZDate date] prettyString],
+                 [[EZDate dateWithYear:2013 month:7 day:4] prettyElapsedTimeString]
                      ];
 }
 
@@ -39,7 +99,7 @@
 
 - (NSInteger)tableView:(UITableView *)tableView numberOfRowsInSection:(NSInteger)section
 {
-    return _codes.count;
+    return _results.count;
 }
 
 - (UITableViewCell *)tableView:(UITableView *)tableView cellForRowAtIndexPath:(NSIndexPath *)indexPath
@@ -47,8 +107,8 @@
     static NSString *CellIdentifier = @"Cell";
     UITableViewCell *cell = [tableView dequeueReusableCellWithIdentifier:CellIdentifier forIndexPath:indexPath];
     
-    cell.textLabel.text = [_results[indexPath.row] description];
-    cell.detailTextLabel.text = _codes[indexPath.row];
+    cell.detailTextLabel.text = [_results[indexPath.row] description];
+    cell.textLabel.text = _codes[indexPath.row];
     
     return cell;
 }
